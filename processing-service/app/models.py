aa598775f71272948a938f0e19859b405151a8f5
Rodolfo <rodolfo.sekijima@hypergiant.com>
2024-11-22 23:06:48 +0900

    feat: add processing-service models

--- processing-service/app/models.py
+++ processing-service/app/models.py
@@ -1,39 +1,28 @@
 import uuid
 
-from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 
 from datetime import datetime
 
-class RewardBase(SQLModel):
-    reward_name: str = Field(unique=True, index=True, max_length=255)
-    reward_item: str | None = Field(default=None)
-    reward_qty: int = Field(default=1)
-
-class Reward(RewardBase, table=True):
-    reward_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    
-class RewardCreate(RewardBase):
-    reward_name: str = Field(max_length=255)
-    reward_item: str = Field(min_length=1, max_length=255)
-    reward_qty: int = 1
-
-class QuestBase(SQLModel):
-    auto_claim: bool = Field(default=False)
+class UserQuestRewardBase(SQLModel):
+    user_id: uuid.UUID 
+    quest_id: uuid.UUID
+    status: int = Field(default=0)
     streak: int = Field(default=0)
     duplication: int = Field(default=0)
-    name: str = Field(unique=True, index=True, max_length=255)
-    description: str | None = Field(default=None)
 
-class Quest(QuestBase, table=True):
-    quest_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    reward_id: uuid.UUID = Field(foreign_key="reward.reward_id")
+class UserQuestReward(UserQuestRewardBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
-class QuestCreate(QuestBase):
-    pass
-
-
-class Event(BaseModel):
+class EventBase(SQLModel):
     event_type: str
     user_id: uuid.UUID
     timestamp: datetime
+
+class EventCreate(EventBase):
+    event_type: str
+    user_id: uuid.UUID
+    timestamp: datetime
+
+class Event(EventBase,table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
