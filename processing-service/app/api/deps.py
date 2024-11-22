82c19e38698fb860ce5fdd26a9e57d75a1146777
Rodolfo <rodolfo.sekijima@hypergiant.com>
2024-11-22 23:51:54 +0900

    fix: remove unused code

--- processing-service/app/api/deps.py
+++ processing-service/app/api/deps.py
@@ -6,11 +6,9 @@ from sqlmodel import Session
 
 from app.core.db import engine
 
-
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
 
 
 SessionDep = Annotated[Session, Depends(get_db)]
-
--- processing-service/app/api/main.py
+++ processing-service/app/api/main.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import quests, rewards, utils
+from app.api.routes import utils
 
 api_router = APIRouter()
-api_router.include_router(quests.router, prefix="/quests", tags=["quests"])
-api_router.include_router(rewards.router, prefix="/rewards", tags=["rewards"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
