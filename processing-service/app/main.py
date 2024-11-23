6d53e71cf5e14e95b050405b1f8fc9ef99b16bf4
Rodolfo <rodolfo.sekijima@hypergiant.com>
2024-11-23 22:01:25 +0900

    update: update references to event_queue

--- processing-service/app/main.py
+++ processing-service/app/main.py
@@ -8,7 +8,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
-from app.core.events.consumer import rabbitmq_client
+from app.core.event_queue import rabbitmq_client
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
