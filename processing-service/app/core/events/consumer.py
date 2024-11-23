86e6f831048a0053d1fdc25cc26aedec73eb22d9
Rodolfo <rodolfo.sekijima@hypergiant.com>
2024-11-23 21:59:38 +0900

    feat: convert function to class for event queue

--- processing-service/app/core/events/consumer.py
+++ processing-service/app/core/events/consumer.py
@@ -4,23 +4,45 @@ import logging
 
 from app.core.config import settings
 from app.models import Event
+from app.service.events import process_event
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def rabbitmq_consumer(queue_name: str = "user_events"):
-    """Function to consume messages from RabbitMQ."""
-    connection = await aio_pika.connect_robust(settings.RABBITMQ)
-    channel = await connection.channel()
+class RabbitMQClient:
+    def __init__(self, queue_name: str = "user_events"):
+        self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
 
-    queue = await channel.declare_queue(queue_name)
+    async def connect(self):
+        self.connection = await aio_pika.connect_robust(settings.RABBITMQ)
+        self.channel = await self.connection.channel()
 
-    async def on_message(message: aio_pika.IncomingMessage):
-        async with message.process():
-            data = json.loads(json.loads(message.body))
-            event = Event(**data)
-            logger.info(f"Received event: {event}")
+    async def publish(self, event: Event):
+        if not self.channel:
+            await self.connect()
 
-            
+        await self.channel.default_exchange.publish(
+            aio_pika.Message(body=json.dumps(event.model_dump_json())),
+            routing_key=self.queue_name,
+        )
+        logger.info(f"Published event: {event}")
 
-    await queue.consume(on_message, no_ack=False)
+    async def consume(self):
+        if not self.channel:
+            await self.connect()
+
+        queue = await self.channel.declare_queue(self.queue_name)
+
+        async def on_message(message: aio_pika.IncomingMessage):
+            async with message.process():
+                data = json.loads(json.loads(message.body))
+                event = Event(**data)
+                logger.info(f"Received event: {event}")
+                process_event(event)
+
+
+        await queue.consume(on_message, no_ack=False)
+
+rabbitmq_client = RabbitMQClient()
